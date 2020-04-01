6b94b9465af96724762479bcfd84b5633ca32880
nicholasdstewart <nstewart3600@gmail.com>
2020-04-01 14:43:46 -0400

    All images loaded into thumb-bar

--- main.js
+++ main.js
@@ -6,8 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+const image_file_indices = [1, 2, 3, 4, 5];
+
+//const newImage = document.createElement('img');
+image_file_indices.forEach((element) => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + element + '.jpg');
+    thumbBar.appendChild(newImage);
+})
+//image_file_indices.forEach(element => console.log(element));
+//thumbBar.appendChild(newImage)
 
 /* Wiring up the Darken/Lighten button */
